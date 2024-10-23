f4f1ad6ec9b2d17758902b706683bea0e925d571
rruetman <rruetman@ucsc.edu>
2024-10-22 19:57:53 -0700

    Lab 5 Changes Final

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock-Paper-Scissors
+// An implementation of the game
+// Author: Ruby Ruetmann <rruetman@ucsc.edu>
+// Date: Fall 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Two players gather 
+// Each player extends one hand out
+// Each player makes a fist with their hand
+// Each player shakes their hand 3 times while saying rock-paper-scissors
+// After saying it 3 times, they show both of their hands 
+// Outcome 1:  Player1 shows Paper and Player 2 shows Rock
+  // Player 1 covers Rock and wins
+// Outcome 2:  Player 2 shows Scissors and Player 1 shows Paper
+  // Player 2 cuts paper and wins
+// Outcome 3 : Player1 show Rock and Player 2 shows Scissors
+  // Player 1 Rock crushes scissors and wins
+// Outcome 4:  Both players tie.
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,19 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types & Variables
+// Author: Ruby Ruetmann
+// Date: October 21 2024
 
-// Constants
+// Define Variables 
+var make = "Subaru";
+var model = "Outback";
+var color = "Silver";
+var year = 2010;
 
-// Functions
+// calculate 
+var age = 2024 - year; 
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "<br>");
